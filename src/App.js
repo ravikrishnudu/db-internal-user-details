cd1cb5ebc21f46dfa789a34a9680d78f8bfdbbc4
ravikrishnudu <ravikrishnuduvenna@gmail.com>
2024-10-02 02:58:01 +0530

    Rendering user deatils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setSheetData } from "./redux/dataSlice";
+import "./App.css";
+
+const API = process.env.REACT_APP_API;
 
 function App() {
+  const dispatch = useDispatch();
+  const sheetData = useSelector((state) => state.data.sheetData);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(`${API}/detailsFromGoogleSheet`);
+        const data = await response.json();
+        dispatch(setSheetData(data));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    })();
+  }, [dispatch]);
+
+  if (!sheetData) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Serial No</th>
+          <th>Name</th>
+          <th>Gender</th>
+          <th>Age</th>
+          <th>Mobile</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sheetData.map((row, index) => (
+          <tr key={index}>
+            <td>{row["Serial No"]}</td>
+            <td>{row["Name"]}</td>
+            <td>{row["Gender"]}</td>
+            <td>{row["Age"]}</td>
+            <td>{row["Mobile"]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
